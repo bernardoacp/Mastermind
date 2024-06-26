34c3abba8c1e8f4f0cebcee4e9b753b428b581b7
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2024-06-26 18:38:35 -0300

    Move the logic responsible for evaluating the guess to a method.

--- main.rb
+++ main.rb
@@ -1,6 +1,32 @@
 require "colorize"
 require "pry-byebug"
 
+def evaluate_guess(guess, code)
+  correct_same_pos = 0
+  correct_diff_pos = 0
+  checked = []
+
+  4.times do |i|
+    if guess[i] == code[i]
+      correct_same_pos += 1
+      checked << i
+    end
+  end
+
+  temp_code = code.map(&:clone)
+  temp_guess = guess.map(&:clone)
+  temp_guess.delete_if.with_index { |_, i| checked.include? i }
+  temp_code.delete_if.with_index { |_, i| checked.include? i }
+
+  temp_guess.each do |num|
+    if temp_code.include? num
+      temp_code.delete_at(temp_code.index(num) || temp_code.length)
+      correct_diff_pos += 1
+    end
+  end
+  [correct_same_pos, correct_diff_pos]
+end
+
 colors = %i[
   red
   green
@@ -23,29 +49,3 @@ puts "1: #{'red'.colorize(:red)}, 2: #{'green'.colorize(:green)}, 3: #{'blue'.co
 
 guess = gets.chomp.split
 guess = guess.map { |num| Integer(num) - 1 }
-
-correct_same_pos = 0
-correct_diff_pos = 0
-checked = []
-
-4.times do |i|
-  if guess[i] == code[i]
-    correct_same_pos += 1
-    checked << i
-  end
-end
-
-temp_code = code.map(&:clone)
-temp_guess = guess.map(&:clone)
-temp_guess.delete_if.with_index { |_, i| checked.include? i }
-temp_code.delete_if.with_index { |_, i| checked.include? i }
-
-temp_guess.each do |num|
-  if temp_code.include?(num)
-    temp_code.delete_at(temp_code.index(num) || temp_code.length)
-    correct_diff_pos += 1
-  end
-end
-
-puts correct_same_pos
-puts correct_diff_pos
