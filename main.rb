d32fac8633bfcee0993bb84d0ea810be72cdcd34
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2024-06-26 18:22:47 -0300

    Implement the logic to check the number of correct colors guessed (same position and different position).

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require "colorize"
+require "pry-byebug"
 
 colors = %i[
   red
@@ -9,8 +10,42 @@ colors = %i[
   white
 ]
 
-code = Array.new(6)
+code = Array.new(4)
 code = code.map { Random.rand(6) }
 
 code.each { |num| print "#{colors[num].to_s.colorize(colors[num])} " }
 puts
+
+puts "Make your 4-number guess"
+# rubocop:disable Layout/LineLength
+puts "1: #{'red'.colorize(:red)}, 2: #{'green'.colorize(:green)}, 3: #{'blue'.colorize(:blue)}, 4: #{'yellow'.colorize(:yellow)}, 5: #{'black'.colorize(:black)}, 6: #{'white'.colorize(:white)}"
+# rubocop:enable Layout/LineLength
+
+guess = gets.chomp.split
+guess = guess.map { |num| Integer(num) - 1 }
+
+correct_same_pos = 0
+correct_diff_pos = 0
+checked = []
+
+4.times do |i|
+  if guess[i] == code[i]
+    correct_same_pos += 1
+    checked << i
+  end
+end
+
+temp_code = code.map(&:clone)
+temp_guess = guess.map(&:clone)
+temp_guess.delete_if.with_index { |_, i| checked.include? i }
+temp_code.delete_if.with_index { |_, i| checked.include? i }
+
+temp_guess.each do |num|
+  if temp_code.include?(num)
+    temp_code.delete_at(temp_code.index(num) || temp_code.length)
+    correct_diff_pos += 1
+  end
+end
+
+puts correct_same_pos
+puts correct_diff_pos
