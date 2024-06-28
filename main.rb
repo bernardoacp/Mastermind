e0ced5e4f42b7b183e65ec469dc91e430b05e9bd
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2024-06-28 19:44:43 -0300

    Add logic to let the player choose between making or breaking the code.

--- main.rb
+++ main.rb
@@ -1,103 +1,7 @@
 require "colorize"
 require "pry-byebug"
-
-def generate_code
-  code = Array.new(4)
-  code = code.map { Random.rand(6) }
-end
-
-def evaluate_guess(guess, code)
-  correct_same_pos = 0
-  correct_diff_pos = 0
-  checked = []
-
-  4.times do |i|
-    if guess[i] == code[i]
-      correct_same_pos += 1
-      checked << i
-    end
-  end
-
-  temp_code = code.map(&:clone)
-  temp_guess = guess.map(&:clone)
-  temp_guess.delete_if.with_index { |_, i| checked.include? i }
-  temp_code.delete_if.with_index { |_, i| checked.include? i }
-
-  temp_guess.each do |num|
-    if temp_code.include? num
-      temp_code.delete_at(temp_code.index(num) || temp_code.length)
-      correct_diff_pos += 1
-    end
-  end
-  [correct_same_pos, correct_diff_pos]
-end
-
-def player_guess(colors, code)
-  winner = false
-  12.times do |turn|
-    puts "-------- Turn #{turn + 1} --------"
-    puts
-    puts "Make your 4-number guess"
-    # rubocop:disable Layout/LineLength
-    puts "1: #{'red'.colorize(:red)}, 2: #{'green'.colorize(:green)}, 3: #{'blue'.colorize(:blue)}, 4: #{'yellow'.colorize(:yellow)}, 5: #{'black'.colorize(:black)}, 6: #{'white'.colorize(:white)}"
-    # rubocop:enable Layout/LineLength
-    puts
-
-    guess = gets.chomp.split
-    guess = guess.map { |num| Integer(num) - 1 }
-    puts
-
-    print "Your guess:\n\n"
-    guess.each { |num| print "#{colors[num].to_s.colorize(colors[num])} " }
-    print "\n\n"
-
-    feedback = evaluate_guess(guess, code)
-
-    if feedback[0] == 4
-      puts "You won!!!"
-      winner = true
-      break
-    end
-
-    puts "Colored pegs: #{feedback[0]}"
-    puts "White pegs:   #{feedback[1]}"
-    print "\n\n"
-  end
-
-  return if winner
-
-  print "You lost, the correct code was:\n\n"
-  code.each { |num| print "#{colors[num].to_s.colorize(colors[num])} " }
-  puts
-end
-
-def computer_guess(colors, code)
-  guess = Array.new(4, 0)
-
-  print "Computer guess:\n\n"
-  guess.each { |num| print "#{colors[num].to_s.colorize(colors[num])} " }
-  print "\n\n"
-
-  feedback = gets.chomp.split
-  feedback = feedback.map { |num| Integer(num) }
-
-  # binding.pry
-
-  total = feedback.sum
-
-  while total != 4
-    (total..3).each { |num| guess[num] += 1 }
-
-    print "Computer guess:\n\n"
-    guess.each { |num| print "#{colors[num].to_s.colorize(colors[num])} " }
-    print "\n\n"
-
-    feedback = gets.chomp.split
-    feedback = feedback.map { |num| Integer(num) }
-
-    total = feedback.sum if feedback.sum > total
-  end
-end
+require_relative "lib/player_guess"
+require_relative "lib/computer_guess"
 
 colors = %i[
   red
@@ -108,14 +12,21 @@ colors = %i[
   white
 ]
 
-# code = generate_code
+puts "Choose game mode:"
+puts "1: Guess the computer's secret code"
+print "2: Create a secret code for the computer to guess\n\n"
+game_mode = gets
 
-# player_guess(colors, code)
+if game_mode == 1
+  player_guess(colors)
+elsif game_mode == 2
+  # rubocop:disable Layout/LineLength
+  puts "Create your 4-number secret code"
+  puts "1: #{'red'.colorize(:red)}, 2: #{'green'.colorize(:green)}, 3: #{'blue'.colorize(:blue)}, 4: #{'yellow'.colorize(:yellow)}, 5: #{'black'.colorize(:black)}, 6: #{'white'.colorize(:white)}"
+  puts
+  # rubocop:enable Layout/LineLength
+  guess = gets.chomp.split
+  guess = guess.map { |num| Integer(num) - 1 }
 
-code = generate_code
-
-print "Code:\n\n"
-code.each { |num| print "#{colors[num].to_s.colorize(colors[num])} " }
-print "\n\n"
-
-computer_guess(colors, code)
+  computer_guess(colors, code)
+end
