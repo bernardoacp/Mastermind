efaf5b0ee4bffbf91049ede265ab092810eb8408
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2024-06-28 19:51:39 -0300

    Return the feedback array from get_feedback; pass the colors as an argument to print_computer_guess.

--- lib/computer_guess.rb
+++ lib/computer_guess.rb
@@ -1,5 +1,5 @@
 def get_feedback(feedback)
-  puts "Give feedback to the computer's guess"
+  print "Give feedback to the computer's guess\n\n"
 
   print "Colored pegs: "
   feedback[0] = gets.chomp
@@ -8,9 +8,11 @@ def get_feedback(feedback)
   print "White pegs:   "
   feedback[1] = gets.chomp
   feedback[1] = Integer(feedback[1])
+
+  feedback
 end
 
-def print_computer_guess(guess)
+def print_computer_guess(colors, guess)
   print "Computer guess:\n\n"
   guess.each { |num| print "#{colors[num].to_s.colorize(colors[num])} " }
   print "\n\n"
@@ -19,7 +21,7 @@ end
 def computer_guess(colors, code)
   guess = Array.new(4, 0)
 
-  print_computer_guess(guess)
+  print_computer_guess(colors, guess)
 
   feedback = Array.new(2)
   feedback = get_feedback(feedback)
@@ -29,7 +31,7 @@ def computer_guess(colors, code)
   while total != 4
     (total..3).each { |num| guess[num] += 1 }
 
-    print_computer_guess(guess)
+    print_computer_guess(colors, guess)
 
     feedback = get_feedback(feedback)
 
@@ -39,7 +41,7 @@ def computer_guess(colors, code)
   while feedback[0] != 4
     guess.shuffle!
 
-    print_computer_guess(guess)
+    print_computer_guess(colors, guess)
 
     feedback = get_feedback(feedback)
   end
